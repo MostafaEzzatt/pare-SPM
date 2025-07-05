bed27258ab4cabc32a3df47080859d7d8e0ceb1b
MostafaEzzatt <psmostafa@gmail.com>
2025-07-05 22:28:19 +0300

    remove security for now

--- security.js
+++ security.js
@@ -1,6 +1,6 @@
 window.setTimeout(() => {
   if (localStorage.getItem("makesafe") !== "trying") {
-    window.location.href = "https://www.google.com/";
+    // window.location.href = "https://www.google.com/";
     console.log("Disable Safe Browsing");
   }
 }, 2000);
